904e18894ce40ef2b00695a8c0795660196c45a3
Bartek <bartosz.papinski00@gmail.com>
2026-01-13 16:07:50 +0100

    feature/fixed-user-model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from datetime import date
 from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    class Gender(models.TextChoices):
+        MALE = 'M', _('Mężczyzna')
+        FEMALE = 'F', _('Kobieta')
+        OTHER = 'O', _('Inna')
+
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50, blank=True)
+
+    gender = models.CharField(
+        max_length=1, 
+        choices=Gender.choices, 
+        default=Gender.MALE
+    )
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,3 +38,12 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    @property
+    def age(self):
+        if not self.birth_date:
+            return None
+        today = date.today()
+        return today.year - self.birth_date.year - (
+            (today.month, today.day) < (self.birth_date.month, self.birth_date.day)
+        )
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from datetime import date
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -7,7 +8,32 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("email", "password")
+        fields = (
+            "id",
+            "email", 
+            "password", 
+            "first_name", 
+            "gender", 
+            "birth_date", 
+            "bio", 
+            "profile_image",
+            "age"
+        )
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'birth_date': {'required': True},
+        }
 
+    def validate_birth_date(self, value):
+        """
+        Sprawdza, czy użytkownik ma ukończone 18 lat.
+        """
+        if value:
+            today = date.today()
+            age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+            if age < 18:
+                raise serializers.ValidationError("Musisz mieć ukończone 18 lat, aby się zarejestrować.")
+        return value
+    
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
